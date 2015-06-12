5390f93d35655e86e5c41f4e72a081f623a06c8e
Daniel Feusse <dfeusse@gmail.com>
2015-06-12 16:29:31 -0400

    evenn on a friday

---
+++ 06_june/12_extractDomainName.py
@@ -0,0 +1,17 @@
+'''
+Write a function that when given a URL as a string, parses out just the domain name 
+and returns it as a string. For example:
+
+domain_name("http://github.com/carbonfive/raygun") == "github" 
+domain_name("http://www.zombie-bites.com") == "zombie-bites"
+domain_name("https://www.cnet.com") == "cnet"
+'''
+import re
+
+def domain_name(domain):
+	newstring = domain.replace('http://', '').replace('https://', '').replace('www.', '')
+	return newstring.partition('.com')[0]
+
+print domain_name("http://github.com/carbonfive/raygun") == "github" 
+print domain_name("http://www.zombie-bites.com") == "zombie-bites"
+print domain_name("https://www.cnet.com") == "cnet"
