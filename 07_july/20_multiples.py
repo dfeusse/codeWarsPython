90b1e1b12de6e8ea89e1e96051467e81e230bbc2
Daniel Feusse <dfeusse@gmail.com>
2015-07-20 07:19:42 -0400

    playa please

---
+++ 07_july/20_multiples.py
@@ -0,0 +1,30 @@
+'''
+Make a program that takes a value (x) and 
+returns "Bang" if the number is divisible by 3, 
+"Boom" if it is divisible by 5, 
+"BangBoom" if it dividible by 3 and 15, and 
+"Miss" if it isn't divisible by any of them. Note: Your program should only return one value
+
+Ex: Input: 105 --> Output: "BangBoom" Ex: Input: 9 --> Output: "Bang" Ex:Input: 25 --> Output: "Boom"
+'''
+def multiple(x):
+	if x % 3 == 0 and x % 15 == 0:
+		return 'BangBoom'
+	elif x % 5 == 0:
+		return 'Boom'
+	elif x % 3 == 0:
+		return 'Bang'
+	else:
+		return 'Miss'
+
+print (multiple(30), "BangBoom")
+print (multiple(3), "Bang")
+print (multiple(98),"Miss")
+print (multiple(65),"Boom")
+print (multiple(23),"Miss")
+print (multiple(15),"BangBoom")
+
+'''
+def multiple(x):
+    return 'Bang' * (x % 3 == 0) + 'Boom' * (x % 5 == 0) or 'Miss'
+'''
